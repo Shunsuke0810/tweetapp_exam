dd61877bf445f38b8709749fb577fb45864fc683
SHUNSUKE <shunsuke0810@gmail.com>
2022-06-03 14:15:50 +0900

    indexページに投稿一覧機能を追加しました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
   def new
     @post = Post.new
@@ -7,5 +8,8 @@ class PostsController < ApplicationController
   def create
     Post.create(content:params[:post][:content])
     redirect_to new_post_path
+  end
+  def post_params
+    params.require(:post).permit(:post, :content)
   end
 end
