a12993ecd65ea3a6dc6273852bb3bd4478529035
SHUNSUKE <shunsuke0810@gmail.com>
2022-06-04 11:14:58 +0900

    投稿編集画面を実装しました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,11 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-
+  
   def new
     @post = Post.new
   end
-
+  
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -15,14 +15,27 @@ class PostsController < ApplicationController
       render :new
     end
   end
-
+  
   def show
     @post = Post.find(params[:id])
   end
   
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "投稿を編集しました！"
+    else
+      render :edit
+    end
+  end
+  
   private
   
-  def post_params
-    params.require(:post).permit(:content)
-  end
+    def post_params
+      params.require(:post).permit(:content)
+    end
 end
