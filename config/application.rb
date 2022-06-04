2052252187b19aad9c8f08ae96ec25524ccd45e2
SHUNSUKE <shunsuke0810@gmail.com>
2022-06-04 14:37:28 +0900

    htmlと文言を編集しました

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,12 @@
 require_relative 'boot'
 
 require 'rails/all'
-
 Bundler.require(*Rails.groups)
 
 module Postapp
   class Application < Rails::Application
-
     config.load_defaults 6.0
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-
+  end
 end
